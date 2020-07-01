77660951cdc803208b8bc0fa17da64b7a264acf5
Pandakidz2 <67604195+Pandakidz2@users.noreply.github.com>
2020-06-30 18:09:45 -0600

    Update index.js

--- index.js
+++ index.js
@@ -3,18 +3,29 @@ const bot = new Discord.Client();
 
 const token = 'NzI3NTkxNzY4MDY1MDQ4NjU2.XvuFEw.s6FYCfJB-yVmu1T8WDpRfbh_ODE';
 
+const prefix = '!p ';
+
 bot.on('ready', () =>{
     console.log('Bot is online!');
 })
 
-bot.on('message', msg=>{
-    if (msg.content === "Hello"){
-        msg.reply("Hello panda brother!");
+bot.on('message', msg =>{
+    let args = msg.content.substring(PREFIX.length).split(" ");
+    
+    switch(args[0]){
+        case 'smiteall':
+            message.channel.sendMessage('You have all been smitten');
+            break;
+        case 'clear':
+            if(!args[1]){
+                return message.reply('Error, please include how many messages you would like to clear.');
+            }
+            message.channel.bulkdelete(arg[1]);
+            setTimeout(function(){
+                message.reply(arg[1] + ' messages hvae been deleted.');
+            }, 5000);
+            break;
     }
 })
-bot.on('message', msg=>{
-    if (msg.content == "Who is the coolest of them all"){
-        msg.reply('Pandakidz2 is obviously the coolest of them all');
-    }
-})
+
 bot.login(token);
