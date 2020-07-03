4fbade0fd2681236810be28584d8c82da6c6205a
Pandakidz2 <67604195+Pandakidz2@users.noreply.github.com>
2020-07-03 10:00:35 -0600

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const token = 'NzI3NTkxNzY4MDY1MDQ4NjU2.XvuFEw.s6FYCfJB-yVmu1T8WDpRfbh_ODE';
 
 const prefix = '$';
 
+var lastMessage;
 bot.on('ready', () =>{
     console.log('Bot is online!');
 })
@@ -13,17 +14,15 @@ bot.on('message', msg =>{
     let args = msg.content.substring(prefix.length).split(" ");
     
     switch(args[0]){
-        case 'smiteall':
-            msg.channel.sendMessage('You have all been smitten');
+        case 'smite':
+            msg.reply('You have been outsmitted by a yuumi jungle. #Feelsbadman');
             break;
         case 'clear':
             if(!args[1]){
                 return msg.reply('Error, please include how many messages you would like to clear.');
             }
             msg.channel.bulkDelete(args[1]);
-            setTimeout(function(){
-                msg.reply(args[1] + ' messages hvae been deleted.');
-            }, 5000);
+            msg.reply(args[1] + ' messages have been deleted.').then(m => m.delete(3000));
             break;
     }
 })
