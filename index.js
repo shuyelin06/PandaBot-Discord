7ae6efc45e9c0ace9668535b36b67b6197ce4a23
Pandakidz2 <67604195+Pandakidz2@users.noreply.github.com>
2020-07-04 23:42:43 -0600

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const bot = new Discord.Client();
 
 const token = 'NzI3NTkxNzY4MDY1MDQ4NjU2.XvuFEw.s6FYCfJB-yVmu1T8WDpRfbh_ODE';
 
-const prefix = '$';
+const prefix = 'pp';
 
 var lastMessage;
 bot.on('ready', () =>{
@@ -15,15 +15,20 @@ bot.on('message', msg =>{
     
     switch(args[0]){
         case 'smite':
-            msg.reply('You have been outsmitted by a yuumi jungle. #Feelsbadman');
+            msg.reply('Alice has been outsmitted by a yuumi jungle. #Feelsbadman');
             break;
         case 'clear':
             if(!args[1]){
                 return msg.reply('Error, please include how many messages you would like to clear.');
             }
             msg.channel.bulkDelete(args[1]);
-            msg.reply(args[1] + ' messages have been deleted.').then(m => m.delete(3000));
+            msg.reply(args[1] + ' messages have been deleted, just like Alices will to live').then(m => m.delete(3000));
             break;
+        case 'whosucks':
+            return msg.reply("Alice sucks");
+            break;
+        case 'alice':
+            return msg.reply('Alice')
     }
 })
 
