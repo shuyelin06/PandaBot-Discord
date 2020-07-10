59b6e10b47390d62ca64aa3626b7e43aabf2a3fe
Pandakidz2 <67604195+Pandakidz2@users.noreply.github.com>
2020-07-10 14:51:30 -0600

    Update index.js

--- index.js
+++ index.js
@@ -5,39 +5,65 @@ const bot = new Discord.Client();
 
 const token = 'NzI3NTkxNzY4MDY1MDQ4NjU2.XvuFEw.s6FYCfJB-yVmu1T8WDpRfbh_ODE';
 
-const prefix = '#';
+const prefix = '$';
 
-bot.login(token);
 // Allows the bot to go online
 bot.on('ready', () =>{
+    bot.channels.cache.get('716378365216686184').send('I am online!');
     console.log('Bot is online!');
 })
 /* DO NOT EDIT ANYTHING ABOVE THIS CODE. LEAVE THIS CODE AS IS. 
 THIS CODE MUST REMAIN THE SAME.*/
 
 // Josh's Code (Only Josh will edit)
-var lastMessage;
+
+let user = null;
+let queue = [];
+
+const guild = new Discord.Guild(bot);
 
 bot.on('message', msg =>{
     let args = msg.content.substring(prefix.length).split(" ");
-    
     switch(args[0]){
-        case 'smite':
-            msg.reply('Alice has been outsmitted by a yuumi jungle. #Feelsbadman');
+        case 'test':
+            msg.reply('Hello, I am online!');
             break;
         case 'clear':
             if(!args[1]){
                 return msg.reply('Error, please include how many messages you would like to clear.');
             }
             msg.channel.bulkDelete(args[1]);
-            msg.reply(args[1] + ' messages have been deleted, just like Alices will to live').then(m => m.delete(3000));
+            msg.reply(args[1] + ' messages have been deleted');
             break;
-        case 'whosucks':
-            return msg.reply("Alice sucks");
+        case 'q':
+            user = msg.member.user.tag;
+            if(args[1] != null){
+                const a = args[1];
+                if(a == 'help'){
+                    msg.reply('new: Create a new queue \n join: Join an existing queue \n clear: Clear an existing queue');
+                }
+                if(a == 'new'){
+                    queue = [];
+                    queue.push(user);
+                    msg.reply("A new queue has been created!" + "\n Current Queue: " + queue);
+                    user = null;
+                } else if(a == 'join'){
+                    queue.push(user);
+                    msg.reply(user + " has been added to the queue!" + "\n Current Queue: " + queue);
+                    user = null;
+                } else if(a == 'clear'){
+                    queue = [];
+                    msg.reply("Queue has been reset.");
+                }
+            }
+            if(args[1] == null){
+                if(queue.length == 0){
+                    msg.reply("No queue yet. Create a new queue!");
+                } else {
+                    msg.reply("Current queue: " + queue);
+                }
+            }
             break;
-        case 'alice':
-            return msg.reply('Alice sucks');
-
     }
 })
 
@@ -46,3 +72,7 @@ bot.on('message', msg =>{
 
 
 // Alice's Code (Only Alice will edit)
+
+
+//Don't edit anything below this!
+bot.login(token);
