0283835168fe54dafa39d8951470f837a9d73ac2
Pandakidz2 <67604195+Pandakidz2@users.noreply.github.com>
2020-07-13 14:51:42 -0600

    Changing queue command
    Queue() is now a class, so multiple queues can be created.

--- commands/q.js
+++ commands/q.js
@@ -1,67 +1,90 @@
-let queue = [];
-let queueNotify = [];
-let user = null;
+let dict = {
+};
 module.exports = {
     name: 'q',
-    description: 'Create and manage a queue to play games with others!',
+    description: 'A test version of the new queue',
     aliases: ['queue'],
-    guildOnly: true,
-    args: true,
-    usage: 'show, new, join, clear, or remove',
     execute(msg, args){
-        user = msg.member.user;
-        const a = args[0];
+        class Queue {
+            constructor(client){
+                this.list = []
+                this.client = client;
+                this.notify = [];
+            }
+        }
+
         switch(args[0]){
-            case 'show':
-                msg.channel.send("Current queue: \n" + queue);
-                break;
             case 'new':
-                queue = [];
-                queue.push(user.username);
-                queueNotify.push(user);
-                msg.channel.send("@here" + `\nA new queue has been created!\nTo join the queue, type \'$q join\'!`);
-                showQueue();
-                user = null;
+                if(!args[1]) msg.reply("Please provide a name for your queue");
+                else {
+                    for(var key in dict){
+                        if(dict[key].client == msg.author.tag){
+                            msg.reply("You already have a queue! End it to start another");
+                            return;
+                        }
+                    }
+                    if(Object.keys(dict).length == 0){
+                        dict[args[1]] = new Queue(msg.author.tag);
+                        msg.reply("You have created a new queue called " + args[1] + "!");
+                    }
+                    else {
+                        let lengthCheck = 0;
+                        for(var key in dict){
+                            if(key != args[1]) lengthCheck++;
+                        }
+                        if(lengthCheck == Object.keys(dict).length){
+                            dict[args[1]] = new Queue(msg.author.tag);
+                            msg.reply("You have created a new queue called " + args[1] + "!");
+                        }
+                        else msg.reply("A queue with this name already exists!");
+                    }
+                }
+                break;
+            case 'show':
+                if(!args[1] && Object.keys(dict).length != 0){
+                    msg.channel.send(display(dict));
+                } else if (args[1] && keyExists(args[1])){
+                    msg.channel.send(display(dict[args[1]].list));
+                } else {
+                    msg.channel.send("Nothing could be found. Please try again.");
+                }
                 break;
             case 'join':
-                if(!queue.includes(user.username)){
-                    queue.push(user.username);
-                    queueNotify.push(user);
-                    msg.channel.send(queueNotify + "\n" + user.username + " has been added to the queue!");
-                    showQueue();
-                } else {
-                    msg.reply("You are already in the queue!");
-                }
-                user = null;
+                if(!args[1]) msg.reply("Please provide the name of the queue you would like to join.");
+                else if (args[1] && keyExists(args[1])){
+                    dict[args[1]].list.push(msg.author.username);
+                    msg.reply("You have been added to queue " + args[1]);
+                } else msg.reply("Queue could not be found. Please try again.");
                 break;
-            case 'clear':
-                queue = [];
-                queueNotify = [];
-                showQueue();
-                break;
-            case 'remove':
-                let q = queue.indexOf(user.username);
-                delete queue[q];
-                let z = queueNotify.indexOf(user);
-                queueNotify.splice(z);
-                msg.reply("You have been removed from the queue.")
-                showQueue();
-                break;
-        }
-        function showQueue(){
-            if(queue.length == 0){
-                msg.channel.send("No queue yet. Create a new queue with $q new!");
-            } else {
-                let display = []
-                display.push("Current queue: ");
-                queue.forEach(element => display.push(element));
-                msg.channel.send(display);
-            }
         }
     }
 }
 
-/* 
-Add reactions to join queues,
-Make people queued show up as a list
-*/
+function display(list){
+    let display = [];
+    display.push("Current queue: ");
+    if(Array.isArray(list)){
+        for(var key in list){
+            display.push(list[key]);
+            console.log(key);
+        }
+    } 
+    else {
+        for(var key in list){
+            display.push(key);
+            console.log(key);
+        }
+    }
+    display.push("Note: To access a specific queue, type $q show followed by the queue name!");
+    return display;
+}
+
+function inQueue(){
+    
+}
+function keyExists(args){
+    for(var key in dict){
+        if(args == key) return true;
+    }
+    return false;
+}
